5fe56db779439b0163ea9e849449c83da51f6b81
mcelam00 <mcelam00@estudiantes.unileon.es>
2020-02-22 19:56:23 +0100

    Implementado constructor sobrecargado de la clase person.

--- Entrega1/person.cpp
+++ Entrega1/person.cpp
@@ -7,7 +7,9 @@ Person::Person()
 
 Person::Person(string nombre, string apellidos, string DNI)
 {
-
+    this->nombre = nombre;
+    this->apellidos = apellidos;
+    this->DNI = DNI;
 }
 
 Person::~Person()
