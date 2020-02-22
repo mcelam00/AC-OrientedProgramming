b6d94780ae11a34ac84178cc9caffff1b60603c0
mcelam00 <mcelam00@estudiantes.unileon.es>
2020-02-22 17:08:27 +0100

    Atributos de la clase Student definidos.

--- Entrega1/student.h
+++ Entrega1/student.h
@@ -3,8 +3,13 @@
 
 #include "person.h" //incluye su clase padre
 
+
 class Student : public Person //es necesario indicar que public Person es de donde hereda
 {
+private:
+    vector<float> listaNotas; //como hereda de person y ya tiene el include vector lo hereda tambien
+    float notaMedia;
+
 public:
     Student();
     ~Student();
