f0f291714b3da2de4d0bb22a2f3135d04c73cce0
mcelam00 <mcelam00@estudiantes.unileon.es>
2020-02-22 17:53:57 +0100

    Función principal del main creada.

--- Entrega1/main.cpp
+++ Entrega1/main.cpp
@@ -1,5 +1,22 @@
 #include <QCoreApplication>
 
+//
+//Funcion principal del programa, que crea los objetos, pide los datos...
+//
+
+void Calificando()
+{
+
+
+
+
+
+
+
+
+}
+
+
 int main()
 {
     Calificando();
