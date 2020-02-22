cd43308798b26a7a1207a258c189e2ca94874a8e
mcelam00 <mcelam00@estudiantes.unileon.es>
2020-02-22 17:44:42 +0100

    Terminadas Funciones header de teacher.

--- Entrega1/teacher.h
+++ Entrega1/teacher.h
@@ -2,7 +2,7 @@
 #define TEACHER_H
 
 #include "person.h" //incluye su clase padre
-
+#include "student.h" //se van a usar la clase student aquí (vector) entonces hay que indicarlo
 
 class Teacher : public Person
 {
@@ -12,6 +12,20 @@ private:
 public:
     Teacher();
     ~Teacher();
+
+    /* GETTERS Y SETTERS */
+    void setNuevoAlumnoEnLista(Student alumno);
+
+    /* OTRAS FUNCIONES */
+
+    void asignarNota(float nota1);   //sobrecargado para asignar 1, 2 o 3 notas según corresponda
+    void asignarNota(float nota1, float nota2);
+    void asignarNota(float nota1, float nota2, float nota3);
+
+    float obtenerNotaMedia();
+
+    void imprimirListaYMedia();
+    void imprimirAlumnoMejorNota();
 };
 
 #endif // TEACHER_H
