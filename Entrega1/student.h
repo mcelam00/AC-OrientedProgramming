0075ee1dba04c8e76b22d12c9bab9790cfb80328
mcelam00 <mcelam00@estudiantes.unileon.es>
2020-02-22 17:24:36 +0100

    Funciones del header de student definidas.

--- Entrega1/student.h
+++ Entrega1/student.h
@@ -8,11 +8,21 @@ class Student : public Person //es necesario indicar que public Person es de don
 {
 private:
     vector<float> listaNotas; //como hereda de person y ya tiene el include vector lo hereda tambien
-    float notaMedia;
-
+                              //maximo tendra tamaño 2 que es el maximo de notas de un alumno
 public:
     Student();
     ~Student();
+
+    /* GETTERS Y SETTERS*/
+
+    float getNota(int numeroDeNotaEnLista);
+    void setNota(float nota); //en la ultima posicion del vector
+    vector<float> getlistaNotas();//con un for y el primero puedo prescindir de este ¿?
+    int getNumeroTotalDeNotas();
+
+    /* OTRAS FUNCIONES */
+
+    float obtenerNota(int posicion); //es el getter ¿?
 
 
 };
